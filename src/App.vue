5d3ff8fc6bd94a9b9790308aa810550534a33526
CourtHive <charles@courthive.com>
2021-09-20 08:05:36 +0200

    add `tods-competition-factory` example

--- src/App.vue
+++ src/App.vue
@@ -8,71 +8,18 @@ import IRound from "./interface/IRound";
 import { Component, Vue } from "vue-property-decorator";
 import TournamentBracket from "./TournamentBracket.vue";
 
+// import bracketData from "./mockData/singleElimination8.json";
+import { generateBracket } from "./generators/competitionFactory"
+const bracketData = generateBracket(16);
+
 @Component({
   components: {
     TournamentBracket,
   },
 })
+
 export default class App extends Vue {
-  rounds: IRound[] = [
-    //Quarter
-    {
-      matchs: [
-        {
-          id: "match1",
-          winner: "1",
-          team1: { id: "1", name: "Competitor 1", score: 2 },
-          team2: { id: "2", name: "Competitor 2", score: 1 },
-        },
-        {
-          id: "match2",
-          winner: "4",
-          team1: { id: "3", name: "Competitor 3", score: 0 },
-          team2: { id: "4", name: "Competitor 4", score: 2 },
-        },
-        {
-          id: "match3",
-          winner: "5",
-          team1: { id: "5", name: "Competitor 5", score: 2 },
-          team2: { id: "6", name: "Competitor 6", score: 1 },
-        },
-        {
-          id: "match4",
-          winner: "8",
-          team1: { id: "7", name: "Competitor 7", score: 0 },
-          team2: { id: "8", name: "Competitor 8", score: 2 },
-        },
-      ],
-    },
-    //Semi
-    {
-      matchs: [
-        {
-          id: "match5",
-          winner: "4",
-          team1: { id: "1", name: "Competitor 1", score: 1 },
-          team2: { id: "4", name: "Competitor 4", score: 2 },
-        },
-        {
-          id: "match6",
-          winner: "8",
-          team1: { id: "5", name: "Competitor 5", score: 1 },
-          team2: { id: "8", name: "Competitor 8", score: 2 },
-        },
-      ],
-    },
-    //Final
-    {
-      matchs: [
-        {
-          id: "any_match_id",
-          winner: "8",
-          team1: { id: "4", name: "Competitor 4", score: 1 },
-          team2: { id: "8", name: "Competitor 8", score: 3 },
-        },
-      ],
-    },
-  ];
+  rounds: IRound[] = bracketData;
 
   private onMatchClick(matchId: string | number): void {
     alert(`click: ${matchId}`);
